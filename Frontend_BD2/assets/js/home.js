6d6664e1b8bd8883d7bc4134e91e38d3b73003dd
Gabriell-Braga <moneybrghikh@gmail.com>
2023-06-07 12:39:23 -0300

    home

--- Frontend_BD2/assets/js/home.js
+++ Frontend_BD2/assets/js/home.js
@@ -1,35 +1,53 @@
-// window.onload = function() {
+window.onload = function () {
+  // logado?
+  var intervalo = 2000; // Executar a cada 2 segundos
+  verificarLogin();
+  // Iniciar o intervalo
+  var intervalID = setInterval(verificarLogin, intervalo);
 
-//     // logado?
-//     var intervalo = 2000; // Executar a cada 2 segundos
-    
-//     // Iniciar o intervalo
-//     var intervalID = setInterval(verificarLogin, intervalo);
-    
-//     function verificarLogin() {
-        
-//         fetch('http://localhost/backEndBD/auth.php', {
-//             method: 'GET',
-//             //headers: {
-//             //    'Content-Type': 'application/json'
-//            // },
-//             //body: JSON.stringify(dadosFormatados)
-//         })
-//             .then(response => response.json())
-//             .then(data => {
-//                 // Aqui você pode lidar com a resposta da API
-//                 if (data.status === false) {
-//                     window.location.href = "index.html";
-//                 }
-//                 else{
-//                     console.log('logado');
-//                 }
-//                 console.log(data);
-//             })
-//             .catch(error => {
-//                 // Aqui você pode lidar com erros na requisição
-//                 console.error(error);
-//             });
-//     }
-    
-//     };
+  function verificarLogin() {
+    fetch("http://localhost/backEndBD/auth.php", {
+      method: "GET",
+      //headers: {
+      //    'Content-Type': 'application/json'
+      // },
+      //body: JSON.stringify(dadosFormatados)
+    })
+      .then((response) => response.json())
+      .then((data) => {
+        // Aqui você pode lidar com a resposta da API
+        if (data.status === false) {
+          window.location.href = "index.html";
+        } else {
+          verifyType(data.usuario.cpf);
+        }
+      })
+      .catch((error) => {
+        // Aqui você pode lidar com erros na requisição
+        console.error(error);
+      });
+  }
+}
+
+function verifyType(cpf){
+    fetch("http://localhost/backEndBD/info_usuario.php", {
+      method: "POST",
+      //headers: {
+      //    'Content-Type': 'application/json'
+      // },
+      body: JSON.stringify({cpf: cpf})
+    })
+      .then((response) => response.json())
+      .then((data) => {
+        if(data.valor == "Professor"){
+            var tec = document.querySelectorAll('.tec');
+            tec.forEach(element => {
+                element.hidden = true;
+            });
+        }
+      })
+      .catch((error) => {
+        // Aqui você pode lidar com erros na requisição
+        console.error(error);
+      });
+}
